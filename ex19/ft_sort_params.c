af275b3c7bd0f157b5f0c6e8ad3e933b2bc8bb00
Araceli Rojas Arce <arojas-a@car8s6.42barcelona.com>
2024-06-10 09:01:41 +0200

    add files

---
+++ ex19/ft_sort_params.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arojas-a <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 14:00:26 by arojas-a          #+#    #+#             */
+/*   Updated: 2024/06/07 15:04:35 by arojas-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putstr(char *s1)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		ft_putchar(s1[i]);
+		i++;
+	}
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+			i++;
+		if (s1[i] < s2[i] || s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < ac - 1)
+	{
+		j = 1;
+		while (j < ac - 1)
+		{
+			if (ft_strcmp(av[j], av[j + 1]) > 0)
+				ft_swap(&av[j], &av[j + 1]);
+			j++;
+		}
+		i++;
+	}
+	j = 1;
+	while (j < ac)
+	{
+		ft_putstr(av[j]);
+		ft_putchar('\n');
+		j++;
+	}
+	return (0);
+}
---
+++ ex20/ft_strdup.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arojas-a <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 16:01:36 by arojas-a          #+#    #+#             */
+/*   Updated: 2024/06/07 16:23:11 by arojas-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	int		len;
+	char	*dest;	
+
+	len = ft_strlen(src);
+	dest = malloc(sizeof(src) * len);
+	i = 0;
+	if (dest == NULL)
+		return (0);
+	else
+	{
+		while (src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (dest);
+}
